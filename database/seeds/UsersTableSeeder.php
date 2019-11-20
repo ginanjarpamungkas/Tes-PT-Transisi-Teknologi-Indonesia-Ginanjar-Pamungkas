1e9a8cfea109aa10275dcc6c76eab41bb5a69583
Ginanjar Pamungkas <ginanjar69919082@gmail.com>
2019-11-20 19:59:38 +0700

    make seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,15 +2,16 @@
 
 use Illuminate\Database\Seeder;
 
-class UsersTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
+class UsersTableSeeder extends Seeder{
+    public function run(){
+        \DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@transisi.id',
+            'password' => bcrypt('transisi'),
+            'email_verified_at' => '2019-11-20 21:00:00',
+            'remember_token' => bcrypt('transisi'),
+            'created_at' => '2019-11-20 21:00:00',
+            'updated_at' => '2019-11-20 21:00:00',
+        ]);
     }
 }
